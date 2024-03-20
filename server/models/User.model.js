43c787100ac7f85a5360907d9a3da245295ec63c
zeul22 <anandrahul044@gmail.com>
2024-03-20 15:57:02 +0530

    Backend work

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,1 +1,78 @@
-                                                 
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcryptjs from "bcryptjs";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "../../.env",
+});
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowecase: true,
+    trim: true,
+  },
+  fname: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  refreshToken: {
+    type: String,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcryptjs.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcryptjs.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
---
+++ server/routes/user.routes.js
@@ -0,0 +1,6 @@
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/signup").post(data);
